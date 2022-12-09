efb73fe0535a1885a1a1d5e17a931fafce671acc
UBnkantah <90422770+UBnkantah@users.noreply.github.com>
2022-12-09 00:09:00 +0000

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,19 +5,18 @@ import "./Header.css"
 const Header = () => {
   return (
     <div className='header'>
-        <div className='fst-header'>
-            <h1>Bring everyone together to build better products.</h1> 
-            <p> Manage makes it simple for software teams to plan day-to-day 
-                tasks while keeping the larger team goals in view.
-            </p>
-            <div className="button">
-              <button>Get Started</button>
-            </div>
-            
-        </div>
-        <div className='sec-header'>
-            <img src={HeaderImg} alt="header-img" />
-        </div>
+      <div className='fst-header'>
+        <h1>Bring everyone together to build better products.</h1> 
+        <p> Manage makes it simple for software teams to plan day-to-day 
+          tasks while keeping the larger team goals in view.
+        </p>
+        <div className="button">
+          <button>Get Started</button>
+        </div>    
+      </div>
+      <div className='sec-header'>
+          <img src={HeaderImg} alt="header-img" />
+      </div>
     </div>
   )
 }
